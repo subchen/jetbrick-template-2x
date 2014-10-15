3c86d8853bd69e000c7abbda6743fe46074a8879
Guoqiang Chen <subchen@gmail.com>
2014-10-15 17:26:13 +0000

    support spring mvc

---
+++ jetbrick-template-springmvc/src/main/java/jetbrick/template/web/springmvc/JetTemplateView.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2013-2014 Guoqiang Chen, Shanghai, China. All rights reserved.
+ *
+ *   Author: Guoqiang Chen
+ *    Email: subchen@gmail.com
+ *   WebURL: https://github.com/subchen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrick.template.web.springmvc;
+
+import java.util.Locale;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import jetbrick.template.JetTemplate;
+import jetbrick.template.web.JetWebContext;
+import jetbrick.template.web.JetWebEngine;
+import org.springframework.web.servlet.view.AbstractTemplateView;
+
+public final class JetTemplateView extends AbstractTemplateView {
+
+    @Override
+    protected void renderMergedTemplateModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        JetWebContext context = new JetWebContext(request, response, model);
+        JetTemplate template = JetWebEngine.getEngine().getTemplate(getUrl());
+        template.render(context, response.getOutputStream());
+    }
+
+    @Override
+    public boolean checkResource(Locale locale) throws Exception {
+        return JetWebEngine.getEngine().checkTemplate(getUrl());
+    }
+}
---
+++ jetbrick-template-springmvc/src/main/java/jetbrick/template/web/springmvc/JetTemplateViewResolver.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2013-2014 Guoqiang Chen, Shanghai, China. All rights reserved.
+ *
+ *   Author: Guoqiang Chen
+ *    Email: subchen@gmail.com
+ *   WebURL: https://github.com/subchen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrick.template.web.springmvc;
+
+import java.beans.ConstructorProperties;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+import jetbrick.template.JetEngine;
+import jetbrick.template.web.JetWebEngine;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.web.servlet.view.AbstractTemplateViewResolver;
+
+public final class JetTemplateViewResolver extends AbstractTemplateViewResolver implements InitializingBean {
+    private String configLocation;
+    private Properties configProperties;
+
+    public JetTemplateViewResolver() {
+        setViewClass(requiredViewClass());
+    }
+
+    public void setConfigLocation(String configLocation) {
+        this.configLocation = configLocation;
+    }
+
+    public void setConfigProperties(Properties configProperties) {
+        this.configProperties = configProperties;
+    }
+
+    @Override
+    protected Class<?> requiredViewClass() {
+        return JetTemplateView.class;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        JetEngine engine = JetWebEngine.create(getServletContext(), configProperties, configLocation);
+        if (getSuffix() == null || getSuffix().length() == 0) {
+            setSuffix(engine.getConfig().getTemplateSuffix());
+        }
+    }
+}
