c03da64f6e5cad0e95153bce51af9de33e6449a8
Guoqiang Chen <subchen@gmail.com>
2014-10-15 17:22:50 +0000

    support jfinal web

---
+++ jetbrick-template-jfinal/src/main/java/jetbrick/template/web/jfinal/JetTemplateRender.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2013-2014 Guoqiang Chen, Shanghai, China. All rights reserved.
+ *
+ *   Author: Guoqiang Chen
+ *    Email: subchen@gmail.com
+ *   WebURL: https://github.com/subchen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrick.template.web.jfinal;
+
+import java.io.IOException;
+import jetbrick.template.JetEngine;
+import jetbrick.template.JetTemplate;
+import jetbrick.template.web.JetWebContext;
+import jetbrick.template.web.JetWebEngine;
+import com.jfinal.core.JFinal;
+import com.jfinal.render.Render;
+
+public final class JetTemplateRender extends Render {
+    private static final long serialVersionUID = 1L;
+
+    public JetTemplateRender(String view) {
+        this.view = view;
+    }
+
+    @Override
+    public void render() {
+        JetEngine engine = JetWebEngine.getEngine();
+        if (engine == null) {
+            engine = JetWebEngine.create(JFinal.me().getServletContext());
+        }
+
+        JetWebContext context = new JetWebContext(request, response);
+        JetTemplate template = engine.getTemplate(view);
+
+        try {
+            template.render(context, response.getOutputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
---
+++ jetbrick-template-jfinal/src/main/java/jetbrick/template/web/jfinal/JetTemplateRenderFactory.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2013-2014 Guoqiang Chen, Shanghai, China. All rights reserved.
+ *
+ *   Author: Guoqiang Chen
+ *    Email: subchen@gmail.com
+ *   WebURL: https://github.com/subchen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrick.template.web.jfinal;
+
+import jetbrick.template.JetEngine;
+import jetbrick.template.web.JetWebEngine;
+import com.jfinal.core.JFinal;
+import com.jfinal.render.IMainRenderFactory;
+import com.jfinal.render.Render;
+
+public final class JetTemplateRenderFactory implements IMainRenderFactory {
+
+    @Override
+    public Render getRender(String view) {
+        return new JetTemplateRender(view);
+    }
+
+    @Override
+    public String getViewExtension() {
+        JetEngine engine = JetWebEngine.getEngine();
+        if (engine == null) {
+            engine = JetWebEngine.create(JFinal.me().getServletContext());
+        }
+        return engine.getConfig().getTemplateSuffix();
+    }
+}
