f63820edb86756272c1128cdfddd015c4f0c8583
Guoqiang Chen <subchen@gmail.com>
2014-11-02 13:29:34 +0000

    let jetbrick webmvc auto scan JetTemplateViewHandler

--- jetbrick-template-jetbrickmvc/src/main/java/jetbrick/template/web/jetbrickmvc/JetTemplateViewHandler.java
+++ jetbrick-template-jetbrickmvc/src/main/java/jetbrick/template/web/jetbrickmvc/JetTemplateViewHandler.java
@@ -17,7 +17,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package jetbrick.template.web.jetbrick;
+package jetbrick.template.web.jetbrickmvc;
 
 import java.io.IOException;
 import java.io.OutputStream;
